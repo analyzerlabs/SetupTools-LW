1d87e62f9ef98af875e8f329208ade7ba43c3ce3
Miguel Quispe <fisicomiguel@gmail.com>
2021-03-24 12:31:00 -0500

    INTERFAZ

---
+++ gui.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMessageBox
+from setuptools import *
+
+Ui_Dialog window() 
+window.setupUi()
+'''
+def dialog():
+    mbox = QMessageBox()
+
+    mbox.setText("Your allegiance has been noted")
+    mbox.setDetailedText("You are now a disciple and subject of the all-knowing Guru")
+    mbox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            
+    mbox.exec_()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(1080,720)
+    w.setWindowTitle('SetupTools')
+    
+    label = QLabel(w)
+    label.setText("Behold the Guru, Guru99")
+    label.move(100,130)
+    label.show()
+
+    btn = QPushButton(w)
+    btn.setText('Beheld')
+    btn.move(110,150)
+    btn.show()
+    btn.clicked.connect(dialog)
+
+    
+    w.show()
+    sys.exit(app.exec_())'''
